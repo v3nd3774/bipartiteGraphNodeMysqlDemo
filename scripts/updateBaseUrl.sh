86a2a9d1b6910a43a3f273a00023ca497b6ab433
v3nd3774 <josue.caraballo@gmail.com>
2023-08-01 16:38:15 -0500

    fix(scripts-+-package): baseUrl

---
+++ scripts/updateBaseUrl.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+#
+# Replaces instances of 'localhost' with live idir url.
+# This enables deployment on server instead of using development url.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$(dirname $SCRIPT_DIR)
+UI_CODE_DIR="$PROJECT_ROOT/react-d3"
+OLD_VAL='"homepage": "/"'
+NEW_VAL='"homepage": "/bipartiteGraphUi"'
+
+echo "OLD_VALUE=${OLD_VAL} /|\ NEW_VALUE=${NEW_VAL}"
+perl -pi.stage-$i.bak -e 's@$OLD_VAL@$NEW_VAL@g' $UI_CODE_DIR/package.json
