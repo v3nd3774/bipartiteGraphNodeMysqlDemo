9a9fb1b992f732a4057cac2c9f7250a269380e82
v3nd3774 <josue.caraballo@gmail.com>
2023-08-01 13:41:15 -0500

    feat(scripts): added deployment url switch on ui

---
+++ scripts/reroute-ui-requests.sh
@@ -0,0 +1,26 @@
+#/usr/bin/env bash
+#
+# Replaces instances of 'localhost' with live idir url.
+# This enables deployment on server instead of using development url.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$(dirname $SCRIPT_DIR)
+UI_CODE_DIR="$PROJECT_ROOT/react-d3/src"
+REPLACE_0=("localhost" "idir.uta.edu")
+REPLACE_1=("5000" "80")
+REPLACE_2=("environ" "bipartiteGraphApi/environ")
+MAIN_ARRAY=(
+  REPLACE_0[@]
+  REPLACE_1[@]
+  REPLACE_2[@]
+)
+
+# https://stackoverflow.com/a/28308205
+COUNT=${#MAIN_ARRAY[@]}
+for ((i=0; i<$COUNT; i++))
+do
+  OLD_VAL=${!MAIN_ARRAY[i]:0:1}
+  NEW_VAL=${!MAIN_ARRAY[i]:1:1}
+  echo "OLD VALUE ${OLD_VAL}"
+  echo "NEW VALUE ${NEW_VAL}"
+  echo "perl -pi.stage-$i.bak -e 's:$OLD_VAL:$NEW_VAL:g' $UI_CODE_DIR/*.js"
+done
