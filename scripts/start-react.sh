48cfa79d25eb9e03558c491abc2725c344b04eb5
v3nd3774 <josue.caraballo@gmail.com>
2023-08-14 15:49:18 -0500

    fix(script): startup for react fixed

--- scripts/start-react.sh
+++ scripts/start-react.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT=$(dirname $SCRIPT_DIR)
 UI_CODE_DIR="$PROJECT_ROOT/react-d3"
 cd $UI_CODE_DIR
+npm install
 npm run build
 npm install -g serve
 # below starts on TCP 3000
