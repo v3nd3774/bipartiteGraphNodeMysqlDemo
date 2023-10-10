0804737b85f99e8e241e566c4347e09c7bf64064
v3nd3774 <josue.caraballo@gmail.com>
2023-10-10 13:53:19 -0500

    fix(config): viewport and default query updated

--- react-d3/src/GraphContext.js
+++ react-d3/src/GraphContext.js
@@ -1,14 +1,14 @@
 import {createContext} from 'react';
  export const defaults = {
   canvas: {
-    height: 2000,
-    width: 2000,
+    height: 4500,
+    width: 4000,
     padding: 0,
     viewBox: {
       o: (-2000 / 30),
-      tw: (-2000 / 30),
-      th: 2000/2,
-      f: 2000*0.6
+      tw: 500,
+      th: 5000,
+      f: 4500
     }
   },
   data: {
@@ -18,7 +18,7 @@ import {createContext} from 'react';
       labelerCol: "username",
       labeleeIdCol: "id",
       labeleeContentCol: "text",
-      joinQuery: "select distinct S.id as id, S.text as text, SU.username as username, A.time as time, A.response as response from Sentence S join Sentence_User SU on S.id = SU.sentence_id join Activity A on SU.sentence_id = A.sentence_id and SU.username = A.username and A.time=SU.time limit 1000;",
+      joinQuery: "select distinct S.id as id, S.text as text, SU.username as username, A.time as time, A.response as response from Sentence S join Sentence_User SU on S.id = SU.sentence_id join Activity A on SU.sentence_id = A.sentence_id and SU.username = A.username and A.time=SU.time where S.id in (select raw.id from (select S.id as id, count(S.id) as cnt from Sentence S join Sentence_User SU on S.id = SU.sentence_id join Activity A on SU.sentence_id = A.sentence_id and SU.username = A.username and A.time=SU.time group by S.id limit 5000) as raw where raw.cnt > 10);",
       timeCol: "time",
       timeOutFormat: "%Y-%m-%d %H:%M:%S"
     },
