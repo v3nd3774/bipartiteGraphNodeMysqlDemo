11b011ca5c194ff61f34c5644a46fb7b2c72be0e
v3nd3774 <josue.caraballo@gmail.com>
2023-11-13 00:06:57 -0600

    feat(sorting): rhs sorting based on user consensus

--- react-d3/src/Sorting.js
+++ react-d3/src/Sorting.js
@@ -12,6 +12,24 @@ function sourceQualitySort (a, b) {
         b.values[0].original.user_quality_score
   );
 }
+function freqs (xs) {
+    const counts = {};
+    for (const x of xs) {
+      counts[x] = counts[x] ? counts[x] + 1 : 1;
+    }
+    return counts
+}
+function targetLabelSort (a, b) {
+
+  const aLabels = a.values.map(d => (d.original.label ? d.original.label : 'ERROR NO LABEL DATA').toString())
+  const bLabels = b.values.map(d => (d.original.label ? d.original.label : 'ERROR NO LABEL DATA').toString())
+  const aFreqs = freqs(aLabels)
+  const bFreqs = freqs(bLabels)
+  // https://www.30secondsofcode.org/js/s/most-frequent-array-element/#:~:text=1%20Use%20Array.prototype.reduce%20%28%29%20to%20map%20unique%20values,get%20the%20most%20frequent%20value%20in%20the%20array.
+  const astr = Object.entries(aFreqs).reduce((a, v) => (v[1] >= a[1] ? v : a), [null, 0])[0]
+  const bstr = Object.entries(bFreqs).reduce((a, v) => (v[1] >= a[1] ? v : a), [null, 0])[0]
+  return astr.localeCompare(bstr)
+}
 function targetNumericalIdSort (a, b) {
   return d3.ascending(parseInt(a.key), parseInt(b.key));
 }
@@ -26,5 +44,7 @@ export const lhsAvailibleSorting = {
 }
 export const rhsAvailibleSorting = {
     "numerical id": targetNumericalIdSort,
-    "reverse numerical id": (a, b) => reverseSortResult(a, b, targetNumericalIdSort)
+    "reverse numerical id": (a, b) => reverseSortResult(a, b, targetNumericalIdSort),
+    "label consensus": targetLabelSort,
+    "reverse label consensus":  (a, b) => reverseSortResult(a, b, targetLabelSort)
 }
