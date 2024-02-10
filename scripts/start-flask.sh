e600911a52aa2a4afec58ec9e241affadcd44b1a
v3nd3774 <josue.caraballo@gmail.com>
2024-02-09 18:31:18 -0600

    fix: 🐛 scripts otel instrument

--- scripts/start-flask.sh
+++ scripts/start-flask.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT=$(dirname $SCRIPT_DIR)
 API_CODE_DIR="$PROJECT_ROOT/db-interface"
 cd $API_CODE_DIR
-python3 app.py
+export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+opentelemetry-instrument --service_name bipartiteGraphApi --logs_exporter otlp flask run -p 5001
