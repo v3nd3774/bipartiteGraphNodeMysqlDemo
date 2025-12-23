963eaaaeaba6c76ad50c97f247d2497da2f6f631
v3nd3774 <josue.caraballo@gmail.com>
2025-12-23 14:21:26 -0600

    feat: 🎸 text icon coloring
    added target label coloring on the RHS of the text label based on simple
    majority

--- react-d3/src/Graph.js
+++ react-d3/src/Graph.js
@@ -248,27 +248,96 @@ export default function Graph () {
           drawReact(layoutData, filterKey, rawData, summaryData, margin)
         }
       })
-    // target labels
-    const tgtlabels = container.append('g')
-      .selectAll('text')
-      .data(targets)
-      .enter().append('text')
+    // // target labels
+    // const tgtlabels = container.append('g')
+    //   .selectAll('text')
+    //   .data(targets)
+    //   .enter().append('text')
 
-    tgtlabels
-      .attr('x', d => d.x)
-      .attr('y', d => d.y + d.height/2)
-          .attr('font-family', 'arial')
-          .attr('font-size', 20)
-          .attr('alignment-baseline', 'middle')
-          .attr('text-anchor', 'middle')
-          .attr('color', 'black')
-          .text(d => {
-            return d.key
-          });
-     tgtlabels
-         .on("mouseenter", (i, g, els) => doSthTgt(i, g, els, rawData, svg, colorScale ))
-         .on("mouseleave", (i, g, els) => stopDoSthTgt(i, g, els, rawData, svg, colorScale ))
+    // var itemIdxToConcensus = {}
+    // tgtlabels.each(d => {
+    //     const itemIdx = d.key
+    //     const edges = d.values
+    //     const originalEdges = edges.map(e => e.original)
+    //     const originalLabels = originalEdges.map(e => e.label)
+    //     const freqs = originalLabels.reduce((countMap, edge) => {
+    //       countMap[edge] = (countMap[edge] || 0) + 1;
+    //       return countMap;
+    //     }, {})
+    //     const entries = Object.entries(freqs)
+    //     const highestEntry = [...entries].reduce((maxEntry, currentEntry) => {
+    //         return currentEntry[1] > maxEntry[1] ? currentEntry : maxEntry;
+    //     });
+    //     itemIdxToConcensus[itemIdx] = highestEntry[0]
+    // })
 
+    // tgtlabels
+    //   .attr('x', d => d.x)
+    //   .attr('y', d => d.y + d.height/2)
+    //       .attr('font-family', 'arial')
+    //       .attr('font-size', 20)
+    //       .attr('alignment-baseline', 'middle')
+    //       .attr('text-anchor', 'middle')
+    //       .attr('color', 'black')
+    //       .text(d => {
+    //         return d.key
+    //       })
+    //       .attr('fill', d => colorScale(itemIdxToConcensus[d.key]))
+    //  tgtlabels
+    //      .on("mouseenter", (i, g, els) => doSthTgt(i, g, els, rawData, svg, colorScale ))
+         //.on("mouseleave", (i, g, els) => stopDoSthTgt(i, g, els, rawData, svg, colorScale ))
+
+// 1. Change selection to 'g' instead of 'text'
+const tgtlabels = container.append('g')
+  .selectAll('g.label-group')
+  .data(targets)
+  .enter().append('g')
+  .attr('class', 'label-group')
+  // Move the group to the target position
+  .attr('transform', d => `translate(${d.x}, ${d.y + d.height/2})`);
+
+var itemIdxToConcensus = {}
+tgtlabels.each(d => {
+    const itemIdx = d.key
+    const edges = d.values
+    const originalEdges = edges.map(e => e.original)
+    const originalLabels = originalEdges.map(e => e.label)
+    const freqs = originalLabels.reduce((countMap, edge) => {
+      countMap[edge] = (countMap[edge] || 0) + 1;
+      return countMap;
+    }, {})
+    const entries = Object.entries(freqs)
+    const highestEntry = [...entries].reduce((maxEntry, currentEntry) => {
+        return currentEntry[1] > maxEntry[1] ? currentEntry : maxEntry;
+    });
+    itemIdxToConcensus[itemIdx] = highestEntry[0]
+})
+
+// 2. Append the text to the groups
+const textElements = tgtlabels.append('text')
+  .attr('font-family', 'arial')
+  .attr('font-size', 20)
+  .attr('alignment-baseline', 'middle')
+  .attr('text-anchor', 'start') // Changed to start so the box follows the text
+  .attr('fill', 'black')
+  .text(d => d.key);
+
+// 3. Append the rectangles using dynamic width
+tgtlabels.append('rect')
+  .attr('x', function(d) {
+    // Finds the sibling text element and measures it
+    const textWidth = d3.select(this.parentNode).select('text').node().getComputedTextLength();
+    return textWidth + 10; // 10px gap
+  })
+  .attr('y', -10) // Center vertically (half of height 20)
+  .attr('width', 20)
+  .attr('height', 20)
+  .attr('fill', d => colorScale(itemIdxToConcensus[d.key]));
+
+// 4. Re-attach events to the group so the whole area is interactive
+tgtlabels
+  .on("mouseenter", (event, d) => doSthTgt(event, d, null, rawData, svg, colorScale))
+  .on("mouseleave", (event, d) => stopDoSthTgt(event, d, null, rawData, svg, colorScale));
 
     let path_str = lines._groups[0][0].getAttribute("d")
     let path_parts = path_str.split(' ')
@@ -276,66 +345,94 @@ export default function Graph () {
     let last_x_int = parseInt(last_x)
 
     function handleZoom(e) {
-      const eventType = e.sourceEvent.type;
-      let cont = d3.select('svg g')
-      let old_transform = cont.attr('transform');
-      let old_scale = 1
-      if (old_transform.includes('scale')) {
-        old_scale = parseFloat(old_transform.split(' ')[1].split(',')[0]);
-      }
-      if (eventType !== 'wheel') {
-        e.transform.k = old_scale
-      }
-      function scaleAndMaintainOffset(domElement, _, __) {
-       let old_transform = domElement.getAttribute("transform")
+      //const eventType = e.sourceEvent.type;
+      //let cont = d3.select('svg g')
+      //let old_transform = cont.attr('transform');
+      //let old_scale = 1
+      //if (old_transform.includes('scale')) {
+      //  old_scale = parseFloat(old_transform.split(' ')[1].split(',')[0]);
+      //}
+      //if (eventType !== 'wheel') {
+      //  e.transform.k = old_scale
+      //}
+      //function scaleAndMaintainOffset(domElement, _, __) {
+      // let old_transform = domElement.getAttribute("transform")
 
-       let new_transform_to_apply_dict = {
-        'scale': [e.transform.k, 1],
-       }
+      // let new_transform_to_apply_dict = {
+      //  'scale': [e.transform.k, 1],
+      // }
 
-       let merged_transform_dict = {}
+      // let merged_transform_dict = {}
 
-       if (!(old_transform == null || old_transform == "" || typeof old_transform == "undefined")) {
-         let prev_transform_dict = old_transform.split(/[^,] [^,]/).reduce((acc, setting) => {
-                let name = setting.split('(')[0]
-                let value = setting.split('(')[1].slice(0,-1)
-                let values = value.split(',')
-                acc[name] = values.map(parseFloat)
-                return acc
-            }, {})
-         merged_transform_dict = Object.assign(merged_transform_dict, prev_transform_dict)
-       }
+      // if (!(old_transform == null || old_transform == "" || typeof old_transform == "undefined")) {
+      //   let prev_transform_dict = old_transform.split(/[^,] [^,]/).reduce((acc, setting) => {
+      //          let name = setting.split('(')[0]
+      //          let value = setting.split('(')[1].slice(0,-1)
+      //          let values = value.split(',')
+      //          acc[name] = values.map(parseFloat)
+      //          return acc
+      //      }, {})
+      //   merged_transform_dict = Object.assign(merged_transform_dict, prev_transform_dict)
+      // }
 
-       merged_transform_dict = Object.assign(merged_transform_dict, new_transform_to_apply_dict)
+      // merged_transform_dict = Object.assign(merged_transform_dict, new_transform_to_apply_dict)
 
-       if ('scale' in merged_transform_dict && 'translate' in merged_transform_dict) {
-            let scale = merged_transform_dict['scale']
-            let translate = merged_transform_dict['translate']
-            let original_translate = parseFloat(d3.select(domElement).attr("data-original-translate-x"))
-            translate[0] = original_translate
-            let new_translate = translate.map((val, idx) => val * scale[idx])
-            merged_transform_dict['translate'] = new_translate
-       }
+      // if ('scale' in merged_transform_dict && 'translate' in merged_transform_dict) {
+      //      let scale = merged_transform_dict['scale']
+      //      let translate = merged_transform_dict['translate']
+      //      let original_translate = parseFloat(d3.select(domElement).attr("data-original-translate-x"))
+      //      translate[0] = original_translate
+      //      let new_translate = translate.map((val, idx) => val * scale[idx])
+      //      merged_transform_dict['translate'] = new_translate
+      // }
 
-       let transform_str = Object.entries(merged_transform_dict).map(([name, values]) => {
-            return `${name}(${values.join(',')})`
-        }).join(' ')
+      // let transform_str = Object.entries(merged_transform_dict).map(([name, values]) => {
+      //      return `${name}(${values.join(',')})`
+      //  }).join(' ')
 
-       d3.select(domElement).attr("transform", transform_str);
-      }
+      // d3.select(domElement).attr("transform", transform_str);
+      //}
 
-      tgtlabels
-        .attr('x', d => last_x_int / e.transform.k)
+      //tgtlabels
+      //  .attr('x', d => last_x_int / e.transform.k)
 
-      srclabels._groups[0].forEach(scaleAndMaintainOffset)
-      tgtlabels._groups[0].forEach(scaleAndMaintainOffset)
+      //srclabels._groups[0].forEach(scaleAndMaintainOffset)
+      //tgtlabels._groups[0].forEach(scaleAndMaintainOffset)
 
-      cont
-          .attr("transform", `scale(1, ${e.transform.k}) translate(0, ${e.transform.y})`);
-      setConfig(updateConfig("zoomLevel", e.transform.k, newConfig))
+      //cont
+      //    .attr("transform", `scale(1, ${e.transform.k}) translate(0, ${e.transform.y})`);
+      //setConfig(updateConfig("zoomLevel", e.transform.k, newConfig))
+            //
+
+const currentK = e.transform.k;
+  const cont = d3.select('svg g');
+
+  // 1. Scale the main container
+  cont.attr("transform", `translate(0, ${e.transform.y}) scale(1, ${currentK})`);
+
+  // 2. Faster text growth math
+  // We use currentK directly now.
+  // If it's STILL too slow, you can use (currentK * 1.5)
+  const labelScale = currentK;
+
+  tgtlabels.attr('transform', d => {
+    const centerY = d.y + d.height / 2;
+
+    // Logic:
+    // X scale = labelScale (Grows 1:1 with zoom)
+    // Y scale = labelScale / currentK (Grows 1:1 with zoom, but cancels parent stretch)
+    return `translate(${d.x}, ${centerY}) scale(${labelScale}, ${labelScale / currentK})`;
+  });
+
+  srclabels.attr('transform', d => {
+    return `translate(${d.x}, ${d.y + d.height / 2}) scale(${labelScale}, ${labelScale / currentK})`;
+  });
+
+  setConfig(updateConfig("zoomLevel", currentK, newConfig));
+
     }
     let zoom = d3.zoom()
-      .scaleExtent([0.1, 5])
+      .scaleExtent([0.1, 10])
       .on('zoom', handleZoom)
 
     d3.select('svg')
